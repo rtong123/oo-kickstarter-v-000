bee4be58eb48f22c218e68477df6398b300bdb80
Rebecca Tong <rtong@buffalo.edu>
2017-11-30 02:57:25 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,6 +9,6 @@ attr_accessor :name, :backed_projects
 
   def back_project(project)
     @backed_projects << project
-    @backed_projects.add_backer
+    @backed_projects = Projects.add_backer
   end
 end
